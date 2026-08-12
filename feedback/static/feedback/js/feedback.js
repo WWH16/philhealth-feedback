d1cc8b8924b4b9524efa8c890f2ddb306f184517
John Ansley Rocel <estevesjancen388@gmail.com>
2026-08-12 17:53:41 +0800

    fix the date and time picker

--- feedback/static/feedback/js/feedback.js
+++ feedback/static/feedback/js/feedback.js
@@ -7,14 +7,14 @@ function getCookie(name) {
 
 function updateDateTime() {
   var dtInput = document.getElementById('dateTime');
-  if (dtInput) {
+  if (dtInput && !dtInput.value) {
     var now = new Date();
     var year = now.getFullYear();
     var month = String(now.getMonth() + 1).padStart(2, '0');
     var day = String(now.getDate()).padStart(2, '0');
     var hours = String(now.getHours()).padStart(2, '0');
     var mins = String(now.getMinutes()).padStart(2, '0');
-    dtInput.value = year + '-' + month + '-' + day + ' ' + hours + ':' + mins;
+    dtInput.value = year + '-' + month + '-' + day + 'T' + hours + ':' + mins;
   }
 }
 
